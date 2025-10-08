e2ffc307ee5989da5c9283180ecd130903ad6ab4
Luz Ximena Rossette Pérez <xime08rossette@hotmail.com>
2025-10-07 23:46:46 -0600

    Añadi un controlador para el perfil de los usuarios (falta hacer la conexion con la base de datos) e igual cree una clase para el perfil del usuario

---
+++ eSaludCareUsers/Controllers/UsuariosController.cs
@@ -0,0 +1,44 @@
+﻿using eSaludCareUsers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+
+namespace eSaludCareUsers.Controllers
+{
+    public class UsuariosController : ApiController
+    {
+        private readonly AppDbContext _context = new AppDbContext();
+
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("api/usuarios")]
+        public IHttpActionResult ObtenerUsuarios()
+        {
+            var pacientes = _context.Pacientes
+                .Select(p => new PerfilUsuario
+                {
+                    Id = p.Id,
+                    Nombre = p.Nombres + " " + p.Apellido,
+                    Telefono = p.Telefono,
+                    Rol = "Paciente"
+                }).ToList();
+
+            var medicos = _context.Medicos
+                .Select(m => new PerfilUsuario
+                {
+                    Id = m.Id,
+                    Nombre = m.Nombre,
+                    Telefono = m.Telefono,
+                    Rol = "Medico"
+                }).ToList();
+
+            var perfiles = pacientes.Concat(medicos).ToList();
+            return Ok(perfiles);
+        }
+
+    }
+}

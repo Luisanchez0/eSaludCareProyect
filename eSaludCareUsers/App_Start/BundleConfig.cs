59147d132418134fadceb7e054844c2c067ae035
Luisanchez0 <luis.sanchez77@unach.mx>
2025-10-09 17:15:09 -0600

    _plantilla e index

--- eSaludCareUsers/App_Start/BundleConfig.cs
+++ eSaludCareUsers/App_Start/BundleConfig.cs
@@ -14,6 +14,9 @@ namespace eSaludCareUsers
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new Bundle("~/bundles/complemento").Include(
+                         "~/Scripts/scripts.js"));
+
             // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información sobre los formularios.  De esta manera estará
             // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(

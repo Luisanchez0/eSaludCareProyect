84077a1d745532984960d39ce4d264f3f8118e7f
Luz Ximena Rossette Pérez <xime08rossette@hotmail.com>
2025-10-09 18:06:04 -0600

    Clase Paciente y Clase Medicos

---
+++ eSaludCareUsers/Models/Medicos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eSaludCareUsers.Models
+{
+    public class Medicos
+    {
+        public int id_medico { get; set; }
+        public string nombres { get; set; }
+        public string apellidos { get; set; }
+
+    }
+}
---
+++ eSaludCareUsers/Models/Paciente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eSaludCareUsers.Models
+{
+    public class Paciente
+    {
+        public int id_paciente { get; set; }
+        public string nombres { get; set; }
+        public string apellidos { get; set; }
+        public string telefono { get; set; }
+
+    }
+}

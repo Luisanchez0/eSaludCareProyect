416ee30518cf7c4d28deff89174d39cc813980f7
Luisanchez0 <luis.sanchez77@unach.mx>
2025-10-11 01:17:06 -0600

    bd

---
+++ eSaludCareAdmin/Data/ConectionBD.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using Npgsql;
+
+namespace eSaludCareAdmin.Data
+{
+    public class ConectionBD
+    {
+        private static string StrConect = ConfigurationManager.ConnectionStrings["BDpsql"].ConnectionString;
+
+        public static NpgsqlConnection ObtenerConexion()
+        {
+            var conexion = new NpgsqlConnection(StrConect);
+            conexion.Open();
+            return conexion;
+
+        }
+    }
+}   

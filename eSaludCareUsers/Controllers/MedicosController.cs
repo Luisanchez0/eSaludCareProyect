032e3f0ede54a8709a473488f2e45095df43189c
Luz Ximena Rossette Pérez <xime08rossette@hotmail.com>
2025-10-13 22:30:56 -0600

    Creacion del controlador medicos para la funcion de "Medicos registrados"

---
+++ eSaludCareUsers/Controllers/MedicosController.cs
@@ -0,0 +1,34 @@
+﻿using eSaludCareUsers.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace eSaludCareUsers.Controllers
+{
+    public class MedicosController : ApiController
+    {
+        private readonly AppDbContext _context = new AppDbContext();
+
+        [HttpGet]
+        [Route("api/medicos")]
+        public IHttpActionResult ObtenerMedicos()
+        {
+            var medicos = _context.Medicos
+                .Select(m => new
+                {
+                    Id = m.id_medico,
+                    Nombre = m.nombres + " " + m.apellidos,
+                    Especialidad = m.especialidad,
+                    Telefono = m.telefono,
+                    Correo = m.correo
+                })
+                .ToList();
+
+            return Ok(medicos);
+
+        }
+    }
+}

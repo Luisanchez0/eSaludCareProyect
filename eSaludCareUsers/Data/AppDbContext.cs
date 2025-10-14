c466074852d4376cff9553b8dd23889a7627e591
Luz Ximena Rossette Pérez <xime08rossette@hotmail.com>
2025-10-13 22:05:51 -0600

    Cambios para obtener los medicos registrados

---
+++ eSaludCareUsers/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+
+namespace eSaludCareUsers.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext() : base("name=DefaultConnection") { }
+
+        public DbSet<eSaludCareUsers.Models.Medico> Medicos { get; set;}
+        public DbSet<eSaludCareUsers.Models.Paciente> Pacientes { get; set; }
+
+        }
+    }
--- eSaludCareUsers/Models/Medico.cs
+++ eSaludCareUsers/Models/Medico.cs
@@ -5,11 +5,15 @@ using System.Web;
 
 namespace eSaludCareUsers.Models
 {
-    public class Medicos
+    public class Medico
     {
         public int id_medico { get; set; }
         public string nombres { get; set; }
         public string apellidos { get; set; }
+        public string especialidad { get; set; }
+        public string telefono { get; set; }
+        public string correo { get; set; } 
+
 
     }
 }

e7cfcbe49a824674fedb89bd4266a05ac45d6600
Karla Núñez <Karli_nuher12@hotmail.com>
2025-10-15 14:18:14 -0600

    Agregue modelo usuario y contexto AppDbContext

---
+++ eSaludCareUsers/Models/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace eSaludCareUsers.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Correo { get; set; }
+
+        [Required]
+        public string Contraseña { get; set; }
+
+        [Required]
+        public string Rol { get; set; } // Ejemplo: "Paciente", "Médico", "Administrador"
+
+    }
+}

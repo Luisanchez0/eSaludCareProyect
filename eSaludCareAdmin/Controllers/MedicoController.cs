530b33bb648192c3c9a0c82df4ff722d65b268a8
Luisanchez0 <luis.sanchez77@unach.mx>
2025-10-15 00:33:30 -0600

    Registro hecho

---
+++ eSaludCareAdmin/Controllers/MedicoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace eSaludCareAdmin.Controllers
+{
+    [Authorize] 
+    [RoutePrefix("api/v1/medicos")]
+    public class MedicoController : ApiController
+    {
+
+        [HttpGet]
+        [Route("listar")]
+        public IHttpActionResult GetMedicos()
+        {
+            // Ejemplo simple, luego puedes conectarlo con CapaNegocio
+            return Ok(new
+            {
+                success = true,
+                message = "Acceso autorizado",
+                usuario = User.Identity.Name
+            });
+        }
+
+    }
+}
--- eSaludCareUsers/App_Start/WebApiConfig.cs
+++ eSaludCareUsers/App_Start/WebApiConfig.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
-
+//using System.Web.Http.Cors;
+//instalar Microsoft.AspNet.WebApi.Cors en el nuget package manager
 namespace eSaludCareUsers
 {
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
         {
+            //config.EnableCors(cors);
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(

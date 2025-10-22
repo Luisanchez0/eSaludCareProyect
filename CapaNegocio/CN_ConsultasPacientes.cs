1390fc5bcc7628f244656cd68846d8bd3b65818d
Karla Núñez <Karli_nuher12@hotmail.com>
2025-10-21 18:04:41 -0600

    Cn_Consultas

---
+++ CapaNegocio/CN_ConsultasPacientes.cs
@@ -0,0 +1,22 @@
+﻿using CapaDatos;
+using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    internal class CN_ConsultasPacientes
+    {
+        private CD_ConsultasPacientes datos = new CD_ConsultasPacientes();
+
+        public List<PacienteConsultaDTO> ObtenerPacientesRegistrados()
+        {
+            return datos.ListarPacientes();
+        }
+
+
+    }
+}

268f9bc832d034947c16be3c466056bacd3e3543
Karla-Hdez2 <Karli_nuher12@hotmail.com>
2025-10-24 09:50:08 -0600

    CapaN_Cc

---
+++ CapaNegocio/CN_ConsultasCitas.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using CapaDatos;
+using CapaEntidad;
+
+namespace CapaNegocio
+{
+    public class CN_ConsultasCitas
+    {
+        private CD_ConsultasCitas datos = new CD_ConsultasCitas();
+
+        public List<CitaAgendadaDTO> ObtenerCitasAgendadas()
+        {
+            return datos.ListarCitas();
+        }
+    }
+}

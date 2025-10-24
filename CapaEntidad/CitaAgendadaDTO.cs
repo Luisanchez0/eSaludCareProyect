3ae28199423124368e16531288d870c66315a423
Karla-Hdez2 <Karli_nuher12@hotmail.com>
2025-10-24 09:22:48 -0600

    DtoCA

---
+++ CapaEntidad/CitaAgendadaDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class CitaAgendadaDTO
+    {
+        public int IdCita { get; set; }
+        public string NombrePaciente { get; set; }
+        public string NombreMedico { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Hora { get; set; }
+        public string Especialidad { get; set; }
+        public string Estado { get; set; }
+
+
+    }
+}

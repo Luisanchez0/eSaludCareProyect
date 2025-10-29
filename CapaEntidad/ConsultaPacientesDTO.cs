b1b14cdee65530a8288a8db4eacc6c8abd8f06d1
Karla-Hdez2 <Karli_nuher12@hotmail.com>
2025-10-29 10:03:44 -0600

    campos

--- CapaEntidad/ConsultaPacientesDTO.cs
+++ CapaEntidad/ConsultaPacientesDTO.cs
@@ -12,9 +12,9 @@ namespace CapaEntidad
             public string Nombre { get; set; }
             public string Telefono { get; set; }
             public DateTime FechaNacimiento { get; set; }
-            public string Genero { get; set; }
             public string Direccion { get; set; }
-            public string Correo { get; set; } // si lo incluyes en la consulta
+            public string Correo { get; set; }
+        
     }
 
 }
